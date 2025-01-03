986aee6d98a4266912b74c9970a57d508d3fdf0f
UJH506 <djawlgp456@gmail.com>
2025-01-03 14:49:41 +0900

    프로젝트 생성

---
+++ backend-mock-exam/src/main/java/jihye/backend_mock_exam/BackendMockExamApplication.java
@@ -0,0 +1,13 @@
+package jihye.backend_mock_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMockExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMockExamApplication.class, args);
+	}
+
+}
---
+++ backend-mock-exam/src/test/java/jihye/backend_mock_exam/BackendMockExamApplicationTests.java
@@ -0,0 +1,13 @@
+package jihye.backend_mock_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMockExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
