7dbef21640df6632227e809c997097065bfb8b48
EJH506 <djawlgp456@gmail.com>
2025-01-20 14:38:45 +0900

    sql 수정

--- sql/3. incorrectNote.sql
+++ sql/3. incorrectNote.sql
@@ -10,55 +10,6 @@ CREATE TABLE incorrect_note (
 	subject_id INT NOT NULL,													-- 해당 문제가 속한 주제 ID
 	level INT NOT NULL,															-- 해당 문제의 level
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,								-- 생성일시
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP	-- 수정일시
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,	-- 수정일시
+    UNIQUE (user_id, question_id)
 )
-
-SELECT * FROM users;
-SELECT * FROM QUESTIONS Q WHERE QUESTION_ID =11;
-SELECT question_id FROM incorrect_note WHERE question_id=108;
-DELETE FROM incorrect_note;
-SELECT * FROM incorrect_note WHERE SUBJECT_ID = 1 AND LEVEL = 1 ORDER BY INCORRECT_NOTE_ID DESC ;
-
-select *
-from (SELECT i.incorrect_note_id, i.user_id, i.question_id, i.subject_id, i.level, q.question_text, i.created_at
-	  FROM incorrect_note i, questions q
-	  where i.question_id = q.question_id) d
-where question_text like '%세번째%';
-
-SELECT i.incorrect_note_id, i.user_id, i.question_id, i.subject_id, i.level, q.question_text, i.created_at
-FROM incorrect_note i
-JOIN questions q ON i.question_id = q.question_id
-WHERE q.question_text LIKE '%세번째%';
-
-select * from incorrect_note;
-SELECT * FROM incorrect_note i, questions q where i.question_id = q.question_id;
-SELECT q.question_text a FROM incorrect_note i, questions q where i.question_id = q.question_id;
-
-SELECT level FROM QUESTIONS Q WHERE QUESTION_ID = 108;
-INSERT INTO incorrect_note (user_id, question_id, subject_id, level) VALUES
-(1, 1, 1, 1),
-(1, 4, 1, 1),
-(1, 61, 4, 3),
-(1, 43, 3, 3),
-(1, 11, 1, 3),
-(1, 125, 8, 2),
-(1, 134, 8, 2),
-(1, 65, 4, 1),
-(1, 97, 6, 3),
-(1, 74, 5, 2),
-(1, 25, 2, 2),
-(1, 78, 5, 3),
-(1, 3, 1, 1),
-(1, 6, 1, 2),
-(1, 24, 2, 2),
-(1, 108, 7, 2);
-
-INSERT INTO incorrect_note (question_id, subject_id, level) VALUES
-(108, (SELECT subject_id FROM QUESTIONS WHERE QUESTION_ID = 108), (SELECT LEVEL FROM QUESTIONS WHERE QUESTION_ID = 108));
-
-INSERT INTO incorrect_note (question_id, subject_id, level)
-SELECT q.question_id, q.subject_id, q.level
-FROM questions q
-WHERE q.question_id = 108;
-
--- SELECT level FROM QUESTIONS WHERE QUESTION_ID = 108;
---
+++ sql/4. history.sql
@@ -0,0 +1,31 @@
+
+USE backend_mock_exam;
+
+DROP TABLE IF EXISTS exam_history;
+DROP TABLE IF EXISTS history_items;
+
+CREATE TABLE exam_history (
+	history_id INT AUTO_INCREMENT PRIMARY KEY,									-- 식별자
+	user_id INT NOT NULL,														-- 유저 ID 외래키
+	subject_name VARCHAR(255) NOT NULL,											-- 주제명
+	level INT NOT NULL,															-- 레벨
+	total_questions_count INT NOT NULL,											-- 총 문항 수
+	correct_questions_count INT NOT NULL,										-- 맞힌 문항 수
+	correct_rate DOUBLE NOT NULL,												-- 정답률
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,								-- 생성일시
+	update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP	-- 수정일시
+)
+
+CREATE TABLE history_items (
+	history_items_id INT AUTO_INCREMENT PRIMARY KEY,							-- 식별자
+	history_id INT NOT NULL,													-- 외래키
+	question_id INT NOT NULL,													-- 문제 ID
+	correct_answer_id INT NOT NULL,												-- 정답 ID
+	user_answer_id INT NOT NULL,												-- 사용자의 답 ID
+	is_correct boolean NOT NULL,												-- 맞혔는지 여부
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,								-- 생성일시
+	update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP	-- 수정일시
+)
+
+SELECT * FROM exam_history;
+SELECT * FROM HISTORY_ITEMS HI ;
